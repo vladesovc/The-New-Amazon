f0109c590fc4741b1d6417897bcdd77628295451
Corey <coreyvladesov2023@u.northwestern.edu>
2023-10-27 22:49:38 -0500

    added in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,21 +16,57 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findByPk(req.params.id, { include: Product });
+    res.status(200).json(allCategories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    // update a category by its `id` value
+    const updatedCategory = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'No category returned, please try again' });
+      return;
+    }
+    res.status(200).json({ message: 'Success. Category has been updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-});
+    try {
+      const deleteCategory = await Category.destroy({
+        where: { id: req.params.id },
+      });
+      if (!deleteCategory) {
+        res.status(404).json({ message: 'Error: Not Deleted' });
+        return;
+      }
+      res.status(200).json({ message: 'Success! Cateogry Deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
