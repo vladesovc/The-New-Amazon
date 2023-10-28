726469ccac56b0b4244f3c43f3cad41fb05abb0f
Corey <coreyvladesov2023@u.northwestern.edu>
2023-10-27 23:13:58 -0500

    add all tags, single tag by id, create, update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,22 +3,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: { model: Product, through: ProductTag },
+    });
+    res.json(allTags);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include: { model: Product, through: ProductTag },
+    });
+    if (!singleTag) {
+      res.status(404).json({ message: 'Error: No Tag Returned' });
+      return;
+    }
+    res.json(singleTag);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    // update a tag's name by its `id` value
+    const [updated] = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!updated) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
